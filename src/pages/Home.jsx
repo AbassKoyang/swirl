49da87570861fe41fae405869848b1c16ff6f1b8
Abass Koyang <abasskoyang12345@gmail.com>
2024-02-18 01:46:09 +0100

    add: suheadingcon component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Hero from "../components/Hero"
 import HeroFeaturesGrid from "../components/HeroFeaturesGrid"
+import SubheadingCon from "../components/SubheadingCon"
 
 const Home = () => {
   return (
     <section className="w-full min-h-screen bg-[#141414] max-w-[1500px]">
       <Hero /> 
       <HeroFeaturesGrid />
+      <SubheadingCon heading="FutureTech Features" text='Unlock the Power of' styles='hidden' />
     </section>
   )
 }
