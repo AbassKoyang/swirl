13190de09fdcba2ec318d70df10affc89e70bb73
Abass Koyang <abasskoyang12345@gmail.com>
2024-02-19 23:15:22 +0100

    made features section responsive for laptop screens

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,12 +13,12 @@ const Home = () => {
       <section className="w-full">
       <SubheadingCon heading="Swirl Features" text='Unlock the Power of' styles='hidden' />
       <div className="w-full grid grid-cols-3 border-b border-b-[#262626]">
-        <div className="col-span-1 2xl:pl-[162px] 2xl:pr-[80px] border-r border-r-[#262626] flex flex-col justify-center">
-          <img src={techBlogIcon} alt="Tech blog icon" className="2xl:size-[80px]" />
-          <h3 className="capitalize font-semibold font-kumbh text-white 2xl:text-3xl 2xl:mt-10">Swirl technology blog</h3>
-          <p className="font-normal font-inter text-[#98989A] 2xl:text-[16px] 2xl:mt-[10px]">Stay informed with our blog section dedicated to future technology.</p>
+        <div className="col-span-1 lg:pl-10 pr-[60px] 2xl:pl-[162px] 2xl:pr-[80px] border-r border-r-[#262626] flex flex-col justify-center">
+          <img src={techBlogIcon} alt="Tech blog icon" className="lg:size-[60px] 2xl:size-[80px]" />
+          <h3 className="capitalize font-semibold font-kumbh text-white lg:text-3xl 2xl:text-[40px] lg:mt-10 2xl:mt-[50px]">Swirl technology blog</h3>
+          <p className="font-normal font-inter text-[#98989A] lg:text-[16px]n 2xl:text-lg lg:mt-[10px] 2xl:mt-4">Stay informed with our blog section dedicated to future technology.</p>
         </div>
-        <div className="col-span-2 grid grid-cols-2 gap-5 2xl:p-[80px] 2xl:pr-[162px]">
+        <div className="col-span-2 grid grid-cols-2 gap-5 lg:p-[60px] lg:pr-10 2xl:p-[80px] 2xl:pr-[162px]">
           <FeaturesCard feature='Quantity' description='Over 1,000 articles on emerging tech trends and breakthroughs.' />
           <FeaturesCard feature='Variety' description='Articles cover fields like AI, robotics, biotechnology, and more.' />
           <FeaturesCard feature='Frequency' description='Fresh content added daily to keep you up to date.' />
@@ -26,12 +26,12 @@ const Home = () => {
         </div>
       </div>
       <div className="w-full grid grid-cols-3 border-b border-b-[#262626]">
-        <div className="col-span-1 2xl:pl-[162px] 2xl:pr-[80px] border-r border-r-[#262626] flex flex-col justify-center">
-          <img src={techBlogIcon} alt="Tech blog icon" className="2xl:size-[80px]" />
-          <h3 className="capitalize font-semibold font-kumbh text-white 2xl:text-3xl 2xl:mt-10">Research Insights Blogs</h3>
-          <p className="font-normal font-inter text-[#98989A] 2xl:text-[16px] 2xl:mt-[10px]">Dive deep into future technology concepts with our research section.</p>
+        <div className="col-span-1 lg:pl-10 pr-[60px] 2xl:pl-[162px] 2xl:pr-10 border-r border-r-[#262626] flex flex-col justify-center">
+          <img src={techBlogIcon} alt="Tech blog icon" className="lg:size-[60px] 2xl:size-[80px]" />
+          <h3 className="capitalize font-semibold font-kumbh text-white lg:text-3xl 2xl:text-[40px] lg:mt-10 2xl:mt-[50px]">Research Insights Blogs</h3>
+          <p className="font-normal font-inter text-[#98989A] lg:text-[16px]n 2xl:text-lg lg:mt-[10px] 2xl:mt-4">Dive deep into future technology concepts with our research section.</p>
         </div>
-        <div className="col-span-2 grid grid-cols-2 gap-5 2xl:p-[80px] 2xl:pr-[162px]">
+        <div className="col-span-2 grid grid-cols-2 gap-5 lg:p-[60px] lg:pr-[80px] 2xl:p-[80px] 2xl:pr-[162px]">
           <FeaturesCard feature='Depth' description='500+ research articles for in-depth understanding.' />
           <FeaturesCard feature='Graphics' description='Visual aids and infographics to enhance comprehension.' />
           <FeaturesCard feature='Trends' description='Explore emerging trends in future technology research.' />
