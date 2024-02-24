68c50af8c27e3d56af6a15c288ca4df9ba708010
Abass Koyang <abasskoyang12345@gmail.com>
2024-02-24 23:39:24 +0100

    made resources section responsive on mobiile screen

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -48,22 +48,22 @@ const Home = () => {
       <LandingPageBlogLayout />
       </section>
     <section className='w-full'>
-      <SubheadingCon heading="Unlock Valuable Knowledge with Swirls's Resources" text='Your Gateway to In-Depth Information' styles='w-full md:w-fit flex items-center justify-center gap-5 2xl:gap-[10px] px-[20px] py-4 2xl:px-[24px] 2xl:py-[18px] bg-[#141414] border border-[#262626] rounded-[8px] 2xl:rounded-xl text-[#98989A] font-normal font-inter text-sm 2xl:text-lg z-20 hover:text-white hover:font-medium transition-all duration-200 ease-in-out' buttonText='View All Resources' containerStyles="flex-col lg:flex-row justify-between" />
+      <SubheadingCon heading="Unlock Valuable Knowledge with Swirls's Resources" text='Your Gateway to In-Depth Information' styles='w-full md:w-fit flex items-center justify-center gap-5 2xl:gap-[10px] px-[20px] py-4 2xl:px-[24px] 2xl:py-[18px] mt-[30px] lg:mt-0 bg-[#141414] border border-[#262626] rounded-[8px] 2xl:rounded-xl text-[#98989A] font-normal font-inter text-sm 2xl:text-lg z-20 hover:text-white hover:font-medium transition-all duration-200 ease-in-out' buttonText='View All Resources' containerStyles="flex-col lg:flex-row justify-between" />
       <div className="w-full grid grid-cols-5 px-5 lg:px-10 2xl:px-[162px] bg-[#141414]">
-        <div className="col-span-2 flex flex-col justify-center 2xl:pt-[80px] 2xl:pr-20 border-r border-r-[#262626]">
-          <img src={ebookIcon} alt="Ebook Icon" className="lg:size-[60px] 2xl:size-20 lg:mt-[30px] 2xl:mb-[50px]" />
-          <h3 className="font-kumbh font-semibold text-white lg:text-[30px] 2xl:text-[40px] lg:mb-[10px] 2xl:mb-4">Ebooks</h3>
-          <p className="font-normal font-inter text-[#98989A] lg:text-lg 2xl:text-xl lg:mb-[30px] 2xl:mb-[50px]">Explore our collection of ebooks covering a wide spectrum of future technology topics.</p>
-          <LandingPageButton text='Download Ebooks Now' styles='w-full md:w-fit 2xl:w-full flex items-center justify-center gap-5 2xl:gap-[14px] px-[20px] py-4 2xl:px-[24px] 2xl:py-[18px] bg-[#1A1A1A] border border-[#262626] rounded-[8px] 2xl:rounded-xl text-[#98989A] font-normal font-inter text-sm 2xl:text-lg z-20 hover:text-white hover:font-medium transition-all duration-200 ease-in-out' />
-          <div className="w-fit flex items-center lg:gap-[30px] 2xl:gap-[50px] lg:p-5 2xl:p-[30px] lg:mt-10 2xl:mt-[60px] rounded-xl bg-[#1A1A1A] border border-[#262626]">
+        <div className="col-span-5 lg:col-span-2 flex flex-col justify-center lg:py-[60px] 2xl:py-20 lg:pr-[60px] 2xl:pr-20 border-r border-r-[#262626]">
+          <img src={ebookIcon} alt="Ebook Icon" className="size-[50px] lg:size-[60px] 2xl:size-20 mt-6 lg:mt-[30px] 2xl:mb-[50px]" />
+          <h3 className="font-kumbh font-semibold text-white text-2xl lg:text-[30px] 2xl:text-[40px] mb-1 lg:mb-[10px] 2xl:mb-4">Ebooks</h3>
+          <p className="font-normal font-inter text-[#98989A] text-sm lg:text-lg 2xl:text-xl mb-6 lg:mb-[30px] 2xl:mb-[50px]">Explore our collection of ebooks covering a wide spectrum of future technology topics.</p>
+          <LandingPageButton text='Download Ebooks Now' styles='w-full flex items-center justify-center gap-5 2xl:gap-[14px] px-[20px] py-4 2xl:px-[24px] 2xl:py-[18px] bg-[#1A1A1A] border border-[#262626] rounded-[8px] 2xl:rounded-xl text-[#98989A] font-normal font-inter text-sm 2xl:text-lg z-20 hover:text-white hover:font-medium transition-all duration-200 ease-in-out' />
+          <div className="w-fit flex items-center gap-[30px] 2xl:gap-[50px] py-5 px-[30px] lg:p-5 2xl:p-[30px] mt-5 lg:mt-10 2xl:mt-[60px] rounded-xl bg-[#1A1A1A] border border-[#262626]">
             <div className="">
-              <p className="font-inter lg:text-sm 2xl:text-lg text-[#98989A] 2xl:mb-1">Downloaded by</p>
-              <h5 className="text-white font-inter font-semibold lg:text-xl 2xl:text-2xl">10k+ Users</h5>
+              <p className="font-inter text-sm 2xl:text-lg text-[#98989A] 2xl:mb-1">Downloaded by</p>
+              <h5 className="text-white font-inter font-semibold text-lg lg:text-xl 2xl:text-2xl">10k+ Users</h5>
             </div>
-            <img src={usersAvatar} alt="Users Avatar Grouped together" className="lg:w-[150] h-[56px] 2xl:w-[193px] 2xl:h-[70px]" />
+            <img src={usersAvatar} alt="Users Avatar Grouped together" className="w-[150] h-[56px] 2xl:w-[193px] 2xl:h-[70px]" />
           </div>
         </div>
-        <div className="col-span-3"></div>
+        <div className="col-span-5 lg:col-span-3"></div>
       </div>
     </section>
     </section>
