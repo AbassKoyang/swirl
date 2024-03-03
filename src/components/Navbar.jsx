33fdca7049b3d593ffe710b02a9eee918e675a39
Abass Koyang <abasskoyang12345@gmail.com>
2024-03-03 02:46:13 +0100

    install react-hook-form and handle form validation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 
 const Navbar = () => {
+  const {pathname} = useLocation();
+  console.log(pathname)
   return (
     <header className="w-full flex items-center justify-center bg-[#1A1A1A]">
         <div className="w-full max-w-[1900px] flex items-center justify-between py-7 px-5 md:px-10 2xl:px-[162px] bg-[#1A1A1A]">
@@ -8,7 +10,7 @@ const Navbar = () => {
             <img src="../src/assets/swirl-logo.svg" alt="Swirl Logo" className="size-8" />
             <h1 className="font-kumbh text-2xl font-semibold text-white">Swirl</h1>
         </Link>
-        <Link to='onboarding/signup' className='px-4 py-1.5 rounded-3xl text-white bg-[#FFD11A] text-sm md:text-lg font-medium font-inter'>
+        <Link to='onboarding/signup' className={`px-4 py-1.5 rounded-3xl text-white bg-[#FFD11A] text-sm md:text-lg font-medium font-inter ${pathname === '/onboarding/signup' ? 'hidden' : 'block'}`}>
             Sign in
         </Link>
         </div>
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,14 +1,31 @@
 import SignUpButton from "../components/SignUpButton";
 import { FcGoogle } from "react-icons/fc";
-import { FaDiscord, FaGithub } from "react-icons/fa";
+import { FaChevronRight, FaDiscord, FaGithub } from "react-icons/fa";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 
 const SignUp = () => {
+  const [steps, setSteps] = useState(0);
+  const [email, setEmail] = useState('')
+  const  [method, setMethod] = useState("");
+  const { watch, register, handleSubmit, formState} = useForm();
+  const {errors, isValid} = formState;
+
+
+  const handleEmailButtonSubmit = (e) => {
+    e.preventDefault();
+    handleSubmit(onsubmit)("email");
+    setSteps((prev) => prev + 1);
+
+  }
+
+
   return (
     <secion className="px-5 w-full bg-[#141414] flex flex-col justify-center items-center h-screen">
       <h1 className="text-2xl font-bold font-kumbh text-white">Sign Up</h1>
-      <p className="text-lg text-[#98989A] font-normal max-w-md mt-3 text-center">Once you sign up, your personal feed will be ready to explore.</p>
-      <div className="w-full max-w-72 flex flex-col mt-5">
+      <p className="text-[16px] text-[#98989A] font-normal max-w-sm mt-3 text-center">Once you sign up, your personal feed will be ready to explore.</p>
+      <div className={`w-full max-w-72 flex-col mt-5 ${steps === 0 ? 'flex' : 'hidden'}`}>
         <div className="w-full flex flex-col gap-2">
         <SignUpButton text='Google' icon={<FcGoogle className="size-6" />} />
         <SignUpButton text='Github' icon={<FaGithub className="size-6" />} />
@@ -19,9 +36,37 @@ const SignUp = () => {
           <span className="font-medium text-lg text-white/35">or</span>
           <div className="w-[40%] h-[1px] bg-white/35"></div>
         </div>
-        <form action="">
-          <input type="email" name="email" id="" placeholder="Email Address" className="w-full bg-black/95 rounded-sm p-2 text-sm" />
+        <form onSubmit={handleEmailButtonSubmit} className="w-full flex items-center justify-between bg-black/95 rounded-sm p-2 focus-within:outline focus-within:outline-1 focus-within:outline-white/35 overflow-hidden outline-offset-[2px]">
+          <input 
+            type="email" 
+            name="email" 
+            id="" 
+            placeholder="Email Address" 
+            className="w-full bg-black/95 rounded-sm p-1.5 text-sm text-white/85 border-0 outline-0"
+            onChange={(e) => setEmail(e.target.value)}
+            {...register('email', {
+              required: {
+                value: true,
+                message: 'Please enter your email address.'
+              },
+              pattern:{
+                value: /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/,
+                message: "Please enter a valid email address."
+              },
+            })}
+            />
+          <button 
+            disabled={!isValid} 
+            type="button" 
+            className="disabled:bg-white/70 p-1.5 rounded-sm  bg-white" 
+            onClick={handleEmailButtonSubmit}><FaChevronRight className="size-4 group-disabled:text-gray-500 text-gray-600"/>
+          </button>
         </form>
+        {errors?.email?.message && <p className="text-xs text-red-600 mt-2">{errors.email.message}</p>}
+      </div>
+      <div className={`w-full ${steps === 1 ? 'flex' : 'hidden'}`}>
+
+        <button onClick={() => setSteps((prev) => prev - 1)}>prev</button>
       </div>
     </secion>
   )
