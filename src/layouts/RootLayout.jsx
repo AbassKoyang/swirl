14603f45bf3fdf65ab36e4cfe288976e175ebd78
Abass Koyang <abasskoyang12345@gmail.com>
2024-03-07 00:23:58 +0100

    Update RootLayout.jsx

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -3,9 +3,9 @@ import Navbar from "../components/Navbar";
 
 const RootLayout = () => {
   return (
-    <div className="w-full flex items-center justify-center flex-col min-h-screen bg-[#141414]">
+    <div className="w-full flex items-center justify-center flex-col min-h-screen bg-blue-600">
         <Navbar />
-        <main className="w-full min-h-screen bg-[#141414] flex items-center justify-center flex-col">
+        <main className="w-full min-h-screen bg-[#141414] flex items-center justify-center flex-col mt-20">
             <Outlet />
         </main>
     </div>
