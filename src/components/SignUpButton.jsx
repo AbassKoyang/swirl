a69d7755816058d14b6443f76638ce0d94e9f0d5
Abass Koyang <abasskoyang12345@gmail.com>
2024-03-07 00:24:41 +0100

    Update SignUpButton.jsx

--- src/components/SignUpButton.jsx
+++ src/components/SignUpButton.jsx
@@ -2,7 +2,7 @@
 const SignUpButton = ({text, icon, signin}) => {
 
   return (
-    <button onClick={signin} className="w-full flex items-center justify-center gap-2 px-5 py-2.5 bg-white hover:bg-[#FFD11A] rounded-sm text-black hover:text-white text-sm font-bold transition-all duration-200 ease-in-out">
+    <button onClick={signin} className="w-full flex items-center justify-center gap-4 px-5 py-2.5 bg-white hover:bg-[#FFD11A] rounded-sm text-black hover:text-white text-sm font-bold transition-all duration-200 ease-in-out">
         <p>{text}</p>
         {icon}
     </button>
